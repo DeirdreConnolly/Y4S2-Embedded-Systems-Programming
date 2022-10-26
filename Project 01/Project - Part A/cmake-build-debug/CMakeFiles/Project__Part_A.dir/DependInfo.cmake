a25a2f841dc8a9057e29c2fb0651eabf075c39b1
DeirdreConnolly <55291172+DeirdreConnolly@users.noreply.github.com>
2022-10-26 01:52:35 +0100

    Add files via upload

---
+++ Project 01/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/crc16_ccitt.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/crc16_ccitt.c.obj"
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/exceptions.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/exceptions.c.obj"
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/string.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/string.c.obj"
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/t.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/t.c.obj"
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/uart.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/uart.c.obj"
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/vid.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/vid.c.obj"
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/editor1/xmodem.c" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/editor1/xmodem.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../editor1"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/main.cpp" "C:/Users/Deirdre/College/CIT/Year 4/Semester 2/Embedded Systems Programming/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../editor1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Project 01/Project - Part A/cmake-build-debug/CMakeFiles/Project__Part_A.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project__Part_A.dir/editor1/crc16_ccitt.c.obj"
+  "CMakeFiles/Project__Part_A.dir/editor1/exceptions.c.obj"
+  "CMakeFiles/Project__Part_A.dir/editor1/string.c.obj"
+  "CMakeFiles/Project__Part_A.dir/editor1/t.c.obj"
+  "CMakeFiles/Project__Part_A.dir/editor1/uart.c.obj"
+  "CMakeFiles/Project__Part_A.dir/editor1/vid.c.obj"
+  "CMakeFiles/Project__Part_A.dir/editor1/xmodem.c.obj"
+  "CMakeFiles/Project__Part_A.dir/main.cpp.obj"
+  "Project__Part_A.exe"
+  "Project__Part_A.exe.manifest"
+  "Project__Part_A.pdb"
+  "libProject__Part_A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Project__Part_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
